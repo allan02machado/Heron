736b431c6e4b171b07452fa050f299ddc68bc05e
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-09-27 21:50:05 -0300

    Este commit altera a página inicial do repositório

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Heron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double c;
+
+            Console.WriteLine("Bem-vindo ao Heron.");
+            Console.WriteLine("Pressione ENTER para continuar...");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("Calcule a área de um triângulo qualquer, dadas as medidas dos 3 lados:");
+
+            Console.Write("Lado A: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Lado B: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Lado C: ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            double area1 = a + b + c;
+            double area2 = area1 * 2;
+            
+            double resultado1 = area2 / 3;
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"A área é: {resultado1}");
+            Console.ResetColor();
+
+            double resultado2 = area1 / 2;
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"O semiperímetro é: {resultado2}");
+            Console.ResetColor();                        
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Heron
 Resposta exercício "Heron"
+
+
+**Enunciado**
+
+Calcule a área de um triângulo qualquer, dadas as medidas dos 3 lados. Exiba o semiperímetro e a área.
+
+**Como executar**
+
+*Abra a pasta do programa e digite:*
+
+dotnet run
+
